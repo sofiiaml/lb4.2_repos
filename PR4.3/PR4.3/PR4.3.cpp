ea477520ce695645bef21ef42f18a768054ed0a4
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-10-13 03:58:20 +0300

    first master commit

---
+++ PR4.3/PR4.3/PR4.3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, a, b, c, F;
+	cout << "a ="; cin >> a;
+	cout << "b ="; cin >> b;
+	cout << "c ="; cin >> c;
+	cout << "xp ="; cin >> xp;
+	cout << "xk ="; cin >> xk;
+	cout << "dx ="; cin >> dx;
+	cout << fixed;
+	cout << "------------------------" << endl;
+	cout << "|" << setw(5) << "x" << "     |" << 
+		setw(7) <<"F" << "     |" << endl;
+	cout << "------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x == 0 && b != 0)
+			F = a * pow(x + c, 2) - b;
+		if (x==0 && b == 0)
+			F = x - a / -c;
+		else
+			F = a + x / c;
+		F;
+		cout << "|" << setw(7) << setprecision(3) << x
+			<< "   |" << setw(10) << setprecision(3) << F
+			<< "  |" << endl;
+		x += dx;
+	}
+cout << "------------------------" << endl;
+return 0;
+}
+
---
+++ PR4.5/PR4.5/PR4.5.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x, y;
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		if ((y <= x && y <= -pow(x, 2) + 2 && y >= 0) ||
+			(y <= -pow(x, 2) + 2 && y <= 0 && y >= x))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+	cout << endl << fixed;
+	for (int i = 0; i < 10; i++)
+	{
+		x = 6. * rand() / RAND_MAX - 3;
+		y = 6. * rand() / RAND_MAX - 3;
+		if ((y <= x && y <= -pow(x, 2) + 2 && y >= 0) ||
+			(y <= -pow(x, 2) + 2 && y <= 0 && y >= x))
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+		else
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+	}
+	return 0;
+}
